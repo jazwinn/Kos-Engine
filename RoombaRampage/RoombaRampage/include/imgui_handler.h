06136e751161ac5122a0f4560f239a078ae6553c
Chiu Jun Jie (Desktop) <2301524@sit.singaporetech.edu.sg>
2024-09-12 23:50:33 +0800

    Basic Hierachy Window + Basic Game Component Window

--- RoombaRampage/RoombaRampage/include/imgui_handler.h
+++ RoombaRampage/RoombaRampage/include/imgui_handler.h
@@ -3,6 +3,8 @@
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
+#include<vector>
+#include<string>
 #include <iostream>
 
 class ImGuiHandler
@@ -18,11 +20,18 @@ public:
     void Shutdown();
 
     void DrawDemoWindow(bool& show_demo_window);
-    void DrawCustomWindow(bool& show_demo_window, bool& show_another_window, ImVec4& clear_color);
+    void DrawHierachyWindow(bool& show_demo_window, bool& show_another_window, ImVec4& clear_color);
     void DrawAnotherWindow(bool& show_another_window);
 
 private:
     bool show_demo_window = true;
     bool show_another_window = false;
+
+    bool stringBox = false;
+    char charBuffer[50] = "";  // Char Buffer
+    std::vector<std::string> obj_text_entries;
+    std::vector<bool> deleteButton;
+    std::vector<bool> obj_component_window;
+
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 };
