20f6e3ccf1d21c5be6baff81081b18db26a5fbc8
Elijah <2301530@sit.singaporetech.edu.sg>
2024-09-13 13:19:29 +0800

    Input base done ECS implementation of input to be done

--- RoombaRampage/RoombaRampage/input.h
+++ RoombaRampage/RoombaRampage/input.h
@@ -3,12 +3,32 @@
 #include <glfw3.h>
 #include <string>
 #include <glm.hpp>
+#include "Math/vector2.h"
 
-namespace input {
-	class classInputHelper {
+namespace Input {
+	class classInput {
 	public:
-		static int gvWidth, gvHeight;
-		static double gvFps;
-		static std::string gvTitle;
+		static int lvWidth, lvHeight;
+		static double lvFps, lvDt;
+		static std::string lvWindowTitle;
+		static GLFWwindow* lvWindow;
+		static Vec2 lvMousePos;
+		static bool lvLMB, lvRMB, lvWButton, lvAButton, lvSButton, lvDButton;
+		
+
+
+		static bool funcWindowInit(int width, int height, std::string title);
+		static bool funcSetupGLFW();
+		static void funcErrorCallback(int error, const char* reason);
+		static void funcFrameBuffCallback(GLFWwindow* window, int width, int height); 
+		static void funcKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+		static void funcMouseCallback(GLFWwindow* window, int key, int action, int mods);
+		static void funcMouseScrollCallback(GLFWwindow* window, double xOffset, double yOffset);
+		static void funcMouseMoveCallback(GLFWwindow* window, double posX, double posY);
+
+		static void funcAddEventCallback();
+		static void funcCalcdt(double interval = 1.0);
+
+
 	};
 }
