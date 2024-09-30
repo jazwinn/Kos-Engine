990530efcab9c8b5dd82072900ab7108dc94cdc5
Elijah <2301530@sit.singaporetech.edu.sg>
2024-09-30 15:26:46 +0800

    Removed warnings from Mat2 and Mat3 Renamed all functions and classes in Mat2 and Mat3

--- RoombaRampage/RoombaRampage/Math/Mat2x2.h
+++ RoombaRampage/RoombaRampage/Math/Mat2x2.h
@@ -2,54 +2,54 @@
 #define MAT2x2_H
 #include <algorithm>
 
-namespace Mat2x2 {
-	class classMat2x2 {
+namespace mat2x2 {
+	class Mat2x2 {
 	public:
 		union {
 			struct {
-				float e00, e01;
-				float e10, e11;
+				float m_e00, m_e01;
+				float m_e10, m_e11;
 			};
 
-			float mat[2][2];
-			float row[4];
+			float m_mat[2][2];
+			float m_row[4];
 		};
 
-		classMat2x2() : e00(0.f), e10(0.f), e01(0.f), e11(0.f) {}
+		Mat2x2() : m_e00(0.f), m_e10(0.f), m_e01(0.f), m_e11(0.f) {}
 
-		classMat2x2(float e1, float e2, float e3, float e4) : e00{ e1 }, e10{ e2 }, e01{ e3 }, e11{ e4 } {}
+		Mat2x2(float e1, float e2, float e3, float e4) : m_e00{ e1 }, m_e10{ e2 }, m_e01{ e3 }, m_e11{ e4 } {}
 
 		/*Mat2x2<T> identity(T val) const{
 			return Mat2x2<T>(val, 0, 0, val);
 		}*/
 
-		void mat2Swap(classMat2x2& lhs, classMat2x2& rhs);
+		void m_Mat2Swap(Mat2x2& lhs, Mat2x2& rhs);
 
-		classMat2x2 mat2Transpose() const;
+		Mat2x2 m_Mat2Transpose() const;
 
-		classMat2x2 mat2Abs() const;
+		Mat2x2 m_Mat2Abs() const;
 
-		classMat2x2 mat2Adjunct() const;
+		Mat2x2 m_Mat2Adjunct() const;
 
-		float mat2Determinant() const;
+		float m_Mat2Determinant() const;
 
-		classMat2x2 mat2Inverse() const;
+		Mat2x2 m_Mat2Inverse() const;
 
 
-		classMat2x2& operator=(const classMat2x2& rhs);
+		Mat2x2& operator=(const Mat2x2& rhs);
 
-		classMat2x2& operator*=(const classMat2x2& rhs);
+		Mat2x2& operator*=(const Mat2x2& rhs);
 
-		classMat2x2& operator +=(const classMat2x2& rhs);
+		Mat2x2& operator +=(const Mat2x2& rhs);
 
-		classMat2x2& operator -=(const classMat2x2& rhs);
+		Mat2x2& operator -=(const Mat2x2& rhs);
 	};
 
-	classMat2x2 operator*(const classMat2x2& lhs, const classMat2x2& rhs);
+	Mat2x2 operator*(const Mat2x2& lhs, const Mat2x2& rhs);
 
-	classMat2x2 operator+(const classMat2x2& lhs, const classMat2x2& rhs);
+	Mat2x2 operator+(const Mat2x2& lhs, const Mat2x2& rhs);
 
-	classMat2x2 operator-(const classMat2x2& lhs, const classMat2x2& rhs);
+	Mat2x2 operator-(const Mat2x2& lhs, const Mat2x2& rhs);
 }
 
 
