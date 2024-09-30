d4bd345ab5ced2e14b63e036a745d891b40e025d
Sean Amiel Mallari Tiu <2303398@sit.singaporetech.edu.sg>
2024-10-01 00:55:50 +0800

    Sprite Renders Independently from collider box

--- RoombaRampage/ECS/System/RenderSystem.cpp
+++ RoombaRampage/ECS/System/RenderSystem.cpp
@@ -13,7 +13,6 @@ namespace ecs {
 			== m_vecTransformComponentPtr.end()) {
 			m_vecTransformComponentPtr.push_back((TransformComponent*)ecs->m_ECS_CombinedComponentPool[TYPETRANSFORMCOMPONENT]->m_GetEntityComponent(ID));
 			m_vecSpriteComponentPtr.push_back((SpriteComponent*)ecs->m_ECS_CombinedComponentPool[TYPESPRITECOMPONENT]->m_GetEntityComponent(ID));
-			m_vecColliderComponentPtr.push_back((ColliderComponent*)ecs->m_ECS_CombinedComponentPool[TYPECOLLIDERCOMPONENT]->m_GetEntityComponent(ID));
 		}
 
 	}
@@ -33,19 +32,16 @@ namespace ecs {
 		size_t IndexLast = m_vecSpriteComponentPtr.size() - 1;
 		std::swap(m_vecSpriteComponentPtr[IndexID], m_vecSpriteComponentPtr[IndexLast]);
 		std::swap(m_vecTransformComponentPtr[IndexID], m_vecTransformComponentPtr[IndexLast]);
-		std::swap(m_vecColliderComponentPtr[IndexID], m_vecColliderComponentPtr[IndexLast]);
 
 		//popback the vector;
 		m_vecSpriteComponentPtr.pop_back();
 		m_vecTransformComponentPtr.pop_back();
-		m_vecColliderComponentPtr.pop_back();
 	}
 
 	void RenderSystem::m_Init()
 	{
 		m_SystemSignature.set(TYPETRANSFORMCOMPONENT);
 		m_SystemSignature.set(TYPESPRITECOMPONENT);
-		m_SystemSignature.set(TYPECOLLIDERCOMPONENT);
 	}
 
 	void RenderSystem::m_Update()
@@ -66,14 +62,11 @@ namespace ecs {
 			//SpriteComponent* MovComp = vecSpriteComponentPtr[n];
 			TransformComponent* transform = m_vecTransformComponentPtr[n];
 			SpriteComponent* sprite = m_vecSpriteComponentPtr[n];
-			ColliderComponent* collider = m_vecColliderComponentPtr[n];
+	
 
 			graphicsPipe->modelData.push_back({ transform->m_rotation, glm::vec2{transform->m_scale.m_x, transform->m_scale.m_y}, glm::vec3{transform->m_position.m_x,transform->m_position.m_y, 0} ,sprite->m_imageID, 0 });
 
-			if (collider->m_drawDebug)
-			{
-				graphicsPipe->debugBoxData.push_back({ 0, glm::vec2{collider->m_Size.m_x * transform->m_scale.m_x, collider->m_Size.m_y * transform->m_scale.m_y}, glm::vec3{transform->m_position.m_x + collider->m_OffSet.m_x,transform->m_position.m_y + collider->m_OffSet.m_y, 0} ,collider->m_isCollided, 0 });
-			}
+			
 			
 		}
 
