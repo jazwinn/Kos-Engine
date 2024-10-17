4461b0120a1c4c77a728d6b139f584901596dadb
Rayner <2301449@sit.singaporetech.edu.sg>
2024-10-17 13:26:33 +0800

    physics layer

--- RoombaRampage/Physics/PhysicsLayer.h
+++ RoombaRampage/Physics/PhysicsLayer.h
@@ -1,9 +1,32 @@
 #ifndef PHYSICSLAYERR_H
 #define PHYSICSLAYERR_H
 
+#include "../ECS/Layers.h"
+#include <vector>
 
+const int size = layer::LAYERS::MAXLAYER;
+namespace physicslayer {
+	class PhysicsLayer {
+	public:
+	PhysicsLayer() {
+		// Initialize the collisionMatrix with 'false'
+		collisionMatrix = std::vector<std::vector<bool>>(size, std::vector<bool>(size, false));
+	}
+	void setCollision(int layer1, int layer2, bool value);
+	void printCollisionMatrix() const;
 
-
-
-
+	private:
+		std::vector<std::vector<bool>> collisionMatrix; // 2D collision matrix
+	};
+}
+/*
+        // Print only the upper triangle part
+        for (int j = i; j >= 0; --j) {
+            std::cout << collisionMatrix[i][j] << " ";
+        }
+        for (int j = i + 1 ; j >= 0 ; --j){
+            std::cout << "  "; // Two spaces for each missing element
+        }
+        std::cout << std::endl;
+*/
 #endif
