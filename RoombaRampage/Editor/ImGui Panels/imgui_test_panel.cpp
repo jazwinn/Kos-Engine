15ad12ccbfd2d3a36930219e9bad2240f6dd0c60
ng jaz winn <ngjazwinn@gmail.com>
2024-11-10 20:58:23 +0800

    clean up

--- RoombaRampage/Editor/ImGui Panels/imgui_test_panel.cpp
+++ RoombaRampage/Editor/ImGui Panels/imgui_test_panel.cpp
@@ -15,7 +15,7 @@ prior written consent of DigiPen Institute of Technology is prohibited.
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
-#include "imgui_handler.h"
+#include "Editor.h"
 #include "../ECS/ECS.h"
 #include "../Application/Helper.h"
 #include "../../De&Serialization/json_handler.h"
