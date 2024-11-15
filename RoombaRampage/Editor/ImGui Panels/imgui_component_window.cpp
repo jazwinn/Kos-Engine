9e5d3c503d9e5ac16a7872f5bfa0d0ec5a167f16
Sean Amiel Mallari Tiu <2303398@sit.singaporetech.edu.sg>
2024-11-15 23:46:43 +0800

    Prevent Tile Length from going below 1.

--- RoombaRampage/Editor/ImGui Panels/imgui_component_window.cpp
+++ RoombaRampage/Editor/ImGui Panels/imgui_component_window.cpp
@@ -952,7 +952,7 @@ void gui::ImGuiHandler::m_DrawComponentWindow()
 
                     //std::cout << EditorCamera::calculateWorldCoordinatesFromMouse(ImGui::GetMousePos().x, ImGui::GetMousePos().y).m_y << std::endl;
 
-                    if (ImGui::IsKeyPressed(ImGuiKey_M))
+                    if (ImGui::IsKeyPressed(ImGuiKey_M) && !tmc->m_tilemapFile.empty())
                     {
                         Tilemap::setIndividualTile(transform->m_position, EditorCamera::calculateWorldCoordinatesFromMouse(ImGui::GetMousePos().x, ImGui::GetMousePos().y), tmc);
                     }
